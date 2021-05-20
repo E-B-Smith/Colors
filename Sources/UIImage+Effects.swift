7595bacd02aadc541e97290cdf2f3fed0f2e0274
Stephanie Lee <stephanie.lee@affirm.com>
2021-05-20 11:11:50 -0700

    update helpers to convenience inits

--- Sources/UIImage+Effects.swift
+++ Sources/UIImage+Effects.swift
@@ -38,10 +38,27 @@ extension CGRect {
 extension UIImage {
 
     /**
+     Returns a UIImage from another UIImage.
+
+     - Parameters:
+     - image:             The image to copy.
+     - Returns:             A UIImage.
+     */
+    public convenience init(_ image: UIImage) {
+        if let cgimage = image.cgImage {
+            self.init(cgImage: cgimage, scale: image.scale, orientation: image.imageOrientation)
+        }
+        if let ciimage = image.ciImage {
+            self.init(ciImage: ciimage, scale: image.scale, orientation: image.imageOrientation)
+        }
+        fatalError("Unknown image type: \(image.debugDescription).")
+    }
+
+    /**
      Returns a solid color image with the given rectangle size.
 
      - Parameters:
-       - color:     The color for the image.
+     - color:     The color for the image.
        - size:      The size of the image.
        - radius:    The radius used if drawing rounded corners. Defaults to nil.
      - Returns:     Returns an image with a solid color of the given size.
@@ -120,30 +137,27 @@ extension UIImage {
      Returns an image with a background of given size and color, with a border respecting the given radius.
 
      - Parameters:
-       - icon:             The main image to add a background to.
-       - backgroundColor:  The color for the background.
-       - size:             The size of the background.
-       - radius:           The radius used if drawing rounded corners. Defaults to 0.
+     - icon:             The main image to add a background to.
+     - backgroundColor:  The color for the background.
+     - size:             The size of the background.
+     - radius:           The radius used if drawing rounded corners. Defaults to 0.
      - Returns:            Returns an image with a solid color of the given size.
-    */
-
-    public static func iconWithBackground(_ icon: UIImage, backgroundColor: UIColor, size: CGSize, radius: CGFloat = 0) -> UIImage {
-
+     */
+    public convenience init(icon: UIImage, backgroundColor: UIColor, size: CGSize, radius: CGFloat = 0) {
         let backgroundImage = try? UIImage(color: backgroundColor, size: size, radius: radius)
-        let modifiedImage = try? backgroundImage?.union(icon)
-
-        return modifiedImage ?? icon
+        let modifiedImage: UIImage = (try? backgroundImage?.union(icon)) ?? icon
+        self.init(modifiedImage)
     }
 
     /**
      Returns an image with a border of given size and color, and respecting the given radius.
 
      - Parameters:
-       - icon:            The main image to add a background to.
-       - borderColor:     The color for the background.
-       - size:            The size of the background.
-       - radius:          The radius used if drawing rounded corners. Defaults to 0.
-       - isDashed:        A boolean that states if the drawn border is solid or dashed.
+     - icon:            The main image to add a background to.
+     - borderColor:     The color for the background.
+     - size:            The size of the background.
+     - radius:          The radius used if drawing rounded corners. Defaults to 0.
+     - isDashed:        A boolean that states if the drawn border is solid or dashed.
      - Returns:           Returns an image with a border of the given color and size.
     */
 
